167586e3622fdbb7d3f898d6b3441c106eb63012
StigUK <prusbogdan2@gmail.com>
2018-10-28 02:46:25 +0300

    Main menu design changes  - set the gameplay project on the background in the main menu  - added script to run backround video  - added bootstrap classes  - added wonderful lobster font

--- index.php
+++ index.php
@@ -6,12 +6,30 @@ require_once 'php/db_connection.php';
 
 <head>
     <title><?= APP_NAME ?></title>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
     <link rel="stylesheet" href="main.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="/vendor/bootstrap.min.css">
     <script src="/vendor/jquery-3.3.1.slim.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="/vendor/jquery.backgroundvideo.min.js"></script>
     <script src="/vendor/popper.min.js"></script>
     <script src="/vendor/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            var videobackground = new $.backgroundVideo($('body'), {
+                "align": "centerXY",
+                "width": 1280,
+                "height": 720,
+                "path": "assets/",
+                "filename": "gameplay",
+                "types": ["mp4","ogg","webm"],
+                "preload": true,
+                "autoplay": true,
+                "loop": true
+            });
+        });
+    </script>
 </head>
 <body>
 <header>
@@ -23,14 +41,18 @@ require_once 'php/db_connection.php';
         <h3>
             Welcome <?= '(' . $_SESSION['id'] . ')' . $_SESSION['login'] ?>
         </h3>
+    <div class="container main-menu-div col-4 bg-dark border border-white">
         <a class="btn btn-block btn-success" href="play.php">Play</a>
         <a class="btn btn-block btn-warning" href="scoreboard.php">Scoreboard</a>
         <a class="btn btn-block btn-warning" href="skins.php">Skins</a>
         <a class="btn btn-block btn-danger" href="logout.php">Log out</a>
-    <?php else: ?>
-        <a class="btn btn-block btn-success" href="play.php">Play as Guest</a>
+    </div>
+        <?php else: ?>
+    <div class="container main-menu-div col-4 bg-dark border border-white">
+        <a class="btn btn-block btn-success " href="play.php">Play as Guest</a>
         <a class="btn btn-block btn-primary" href="login.php">Log in</a>
         <a class="btn btn-block btn-primary" href="signup.php">Sign up</a>
+    </div>
     <?php endif; ?>
 </div>
 </body>
