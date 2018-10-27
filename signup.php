5cdc5470e3f7a7bb19786b1ba96c15a5d9994bc2
wenceslaus <esteticwence@gmail.com>
2018-10-27 22:03:40 +0300

    Fix registration

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 
 require_once 'php/db_connection.php';
 
-const ALERT_ERROR="Such username already exists.";
+const ALERT_ERROR = "Such username already exists.";
 
 if (isset($_POST['signup-btn'])) {
     $username = $_POST['username'];
@@ -14,13 +14,14 @@ if (isset($_POST['signup-btn'])) {
     if ($count_row == 0) {
         $query_add = "INSERT INTO User SET username = '$username', password = '$password'";
         $result = mysqli_query($db, $query_add) or die (mysqli_connect_errno() . " Writing error: Username - $username, Password - $password.");
+        header('location: index.php');
     } else $alert = ALERT_ERROR;
 }
 
 ?>
 
 <head>
-    <title><?=APP_NAME?></title>
+    <title><?= APP_NAME ?></title>
     <link rel="stylesheet" href="main.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="/vendor/bootstrap.min.css">
@@ -35,9 +36,9 @@ if (isset($_POST['signup-btn'])) {
 <div>
     <form method="post">
         <label>Username</label>
-        <input name="_login" type="text">
+        <input name="username" type="text">
         <label>Password</label>
-        <input name="_password" type="text">
+        <input name="password" type="text">
         <input name="signup-btn" type="submit" value="Sign Up" class="btn btn-success">
     </form>
     <?php if ($alert == ALERT_ERROR): ?>
